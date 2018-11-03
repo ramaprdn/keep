c8c4a2a2625cff48660065ef11bab5d891fc8ec9
evansaragih <evansaragih@student.unud.ac.id>
2018-11-03 11:57:35 +0800

    tambah detail profil teman

---
+++ app/src/main/java/com/example/ramapradana/keep/DetailFriendActivity.java
@@ -0,0 +1,13 @@
+package com.example.ramapradana.keep;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detailfriend);
+    }
+}
